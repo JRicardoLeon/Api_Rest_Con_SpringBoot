de18497f6adb1c8487dae6c5480318277f8914af
JRicardoLeon <josericardobl1@outlook.com>
2022-03-16 19:52:55 -0500

    se creo el controller para crear la tarea en la DB H2

--- src/main/java/com/Api_Rest/controller/TaskController.java
+++ src/main/java/com/Api_Rest/controller/TaskController.java
@@ -1,7 +1,24 @@
 package com.Api_Rest.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.Api_Rest.persistence.entetys.Task;
+import com.Api_Rest.service.TaskService;
+import com.Api_Rest.service.dto.TaskDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/task")
 public class TaskController {
+
+    @Autowired
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task crearTarea(@RequestBody TaskDTO taskdto){
+    return this.taskService.CreateTask(taskdto);
+    }
 }
